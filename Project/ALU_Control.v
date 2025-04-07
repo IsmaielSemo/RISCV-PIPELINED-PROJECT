009e6e0f33075dc48268fedc53b1ff481be2396b
Ismaiel Sabet <95542909+IsmaielSemo@users.noreply.github.com>
2025-04-07 16:03:27 +0200

    Update ALU_Control.v

--- Project/ALU_Control.v
+++ Project/ALU_Control.v
@@ -3,8 +3,8 @@
 
 module ALUControlUnit (
     input [1:0] ALUOp,      
-    input [2:0] Inst_14_12,  
-    input Inst_30,           
+    input [2:0] function3,  
+    input function1,           
     output reg [3:0] ALU_sel 
 );
 
@@ -15,17 +15,49 @@ always @(*) begin
     case (ALUOp)
         2'b00: ALU_sel  = 4'b0010;  // ADD
         2'b01: ALU_sel  = 4'b0110;  // SUB
-        2'b10: begin      
-            case (Inst_14_12)
-                3'b000: begin     
-                    if (Inst_30 == 1'b0) 
-                        ALU_sel  = 4'b0010; 
-                     else 
-                        ALU_sel  = 4'b0110;         
-                end
-                3'b111:  ALU_sel  = 4'b0000;  // AND         
-                3'b110: ALU_sel  = 4'b0001;  // OR
-
+        2'b10: begin
+            if({function3,function1} == 4'b0000)
+                ALU_sel = 4'b0010; //ADD
+            else if({function3,function1} == 4'b0001)
+                ALU_sel = 4'b0110; //SUB
+            else if({function3,function1} == 4'b1110)
+                ALU_sel = 4'b0000; //AND
+            else if({function3,function1} == 4'b1100)
+                ALU_sel = 4'b0001; //OR
+            else if({function3,function1} == 4'b1000)
+                ALU_sel = 4'b0100; //XOR
+            else if({function3,function1} == 4'b0010)
+                ALU_sel = 4'b0111; //SLLI
+            else if({function3,function1} == 4'b1010)
+                ALU_sel = 4'b1000; //SRLI
+            else if({function3,function1} == 4'b1011)
+                ALU_sel = 4'b1001;   //SRAI
+            else if({function3,function1} == 4'b0100)
+                ALU_sel = 4'b1010;   //SLTI 
+            else if({function3,function1} == 4'b0110)
+                ALU_sel = 4'b1011;   //SLTIU
+        end
+        2'b11: begin //I type instructions
+            if(function3 == 3'b000)
+                ALU_sel = 4'b0010;// AddI
+            else if(function3 == 3'b111)
+                ALU_sel = 4'b0000; //ANDI
+            else if(function3 == 3'b111)
+                ALU_sel = 4'b0000; //ANDI 
+            else if(function3 == 3'b111)
+                ALU_sel = 4'b0000; //ANDI 
+            else if(function3 == 3'b111)
+                ALU_sel = 4'b0000; //ANDI
+            else if(function3 == 3'b111)
+                ALU_sel = 4'b0000; //ANDI 
+            else if(function3 == 3'b111)
+                ALU_sel = 4'b0000; //ANDI 
+            else if(function3 == 3'b111)
+                ALU_sel = 4'b0000; //ANDI
+            else if(function3 == 3'b111)
+                ALU_sel = 4'b0000; //ANDI
+        
+        end
             endcase
         end             
     endcase
