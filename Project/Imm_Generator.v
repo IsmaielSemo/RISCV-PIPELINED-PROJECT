a87a01ed8cade191a22ddbd973285f8f00ca38ab
koussay0 <49121836+koussay0@users.noreply.github.com>
2025-04-09 16:14:23 +0200

    Update Imm_Generator.v

--- Project/Imm_Generator.v
+++ Project/Imm_Generator.v
@@ -2,13 +2,21 @@
 
 
 
-module ImmGen (output [31:0] gen_out, input [31:0] inst);
+module ImmGen (output [31:0] gen_out,output L, input [31:0] inst);
 
 reg [11:0] imm;
 wire sign_bit;
 always @(*)begin
-    if(inst[6:5] == 2'b00) begin //LW
-    imm = inst[31:20];         
+    if(inst[6:5] == 2'b00) begin //Load instruction
+        if(inst[14:12] == 3'b010)begin  //LW    
+            imm = inst[31:20]; L=2;
+        end     
+        if(inst[14:12] == 3'b000)begin //LB
+           imm = inst[31:20]; L=1;   
+        end
+        if(inst[14:12] == 3'b001)begin //LH
+           imm = inst[31:20]; L=1;
+        end   
     end
     else if(inst[6:5] == 2'b01) begin //SW
     imm = {inst[31:25], inst[11:7]}; //7+5=12
